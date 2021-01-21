ac275622a3ffcfaa3b2633ca944ac66a7b7e928a
Mohit Kumar <mohitk02082000@gmail.com>
2021-01-22 00:09:21 +0530

    added functionality to add todo items
    added click listeners on clearBtns

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const body = document.querySelector("body");
 const todoMode = document.querySelector(".todo-mode");
 const todoAdd = document.querySelector(".todo-add");
+const todoItemsContainer = document.querySelector(".todo-items-container");
 
 //set to dark mode if the system is in dark mode
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    // dark mode
-    console.log("dark mode");
-    body.classList.add("dark")
+if (
+	window.matchMedia &&
+	window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+	// dark mode
+	console.log("dark mode");
+	body.classList.add("dark");
 }
 
-// watch for change of system theme 
+// watch for change of system theme
 window
 	.matchMedia("(prefers-color-scheme: dark)")
 	.addEventListener("change", (e) => {
-        // const newColorScheme = e.matches ? "dark" : "light";
-        
-        console.log(e.matches);
+		// const newColorScheme = e.matches ? "dark" : "light";
+
+		console.log(e.matches);
 
 		if (e.matches) {
 			body.classList.add("dark");
@@ -28,7 +32,64 @@ todoMode.addEventListener("click", () => {
 	body.classList.toggle("dark");
 });
 
+todoAdd.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-todoAdd.addEventListener("submit",(e) => {
-	alert("submit");
-})
+	const todoText = todoAdd.querySelector("input");
+
+	makeNewTodo(todoText.value);
+
+
+	todoText.value = "";
+});
+
+const todoItem = (text) => {
+
+	const div = document.createElement("div");
+	div.classList.add("todo-box","todo-item");
+	
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.name = "item-1";
+	checkbox.id = "item-1";
+
+	div.appendChild(checkbox);
+
+	const label = document.createElement("label");
+	label.classList.add("item-label");
+	label.htmlFor = "item-1";
+	const textNode = document.createTextNode(text);
+	label.appendChild(textNode);
+	
+	div.appendChild(label);
+
+	const span = document.createElement("span");
+	span.classList.add("checkmark");
+
+	div.appendChild(span);
+
+	const img = document.createElement("img");
+	img.src = "./images/icon-cross.svg"
+	img.classList.add("clear")
+
+	img.addEventListener("click",(e) => {
+
+		const clearBtn = e.target;
+		console.log(clearBtn);
+		const TodoToDelete = clearBtn.closest(".todo-item");
+
+		TodoToDelete.remove();
+
+	})
+
+	div.appendChild(img);
+
+	return div;
+
+}
+
+
+const makeNewTodo = (todoText) => {
+
+	todoItemsContainer.insertBefore(todoItem(todoText), todoItemsContainer.childNodes[0]);
+};
