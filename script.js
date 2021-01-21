0139e64e740212f79058e1e3b15c092a8ac9848d
Mohit Kumar <mohitk02082000@gmail.com>
2021-01-21 14:31:00 +0530

    implemented dark and light theme
    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+const todoMode = document.querySelector(".todo-mode");
+
+
+//set to dark mode if the system is in dark mode
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // dark mode
+    console.log("dark mode");
+    body.classList.add("dark")
+}
+
+// watch for change of system theme 
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", (e) => {
+        // const newColorScheme = e.matches ? "dark" : "light";
+        
+        console.log(e.matches);
+
+		if (e.matches) {
+			body.classList.add("dark");
+		} else {
+			body.classList.remove("dark");
+		}
+	});
+
+todoMode.addEventListener("click", () => {
+	body.classList.toggle("dark");
+});
+
